34824d6947073390bfebdedfecfe855a1a7ac14c
Marc8s <marcus.ecomp@gmail.com>
2021-03-15 16:17:22 +0000

    Snackbar

--- app/src/main/java/com/example/componentes/MainActivity.kt
+++ app/src/main/java/com/example/componentes/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.componentes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -12,13 +15,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         button_toast.setOnClickListener(this)
+        button_snack.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
         when(v.id){
             R.id.button_toast ->{
                 val toast = Toast.makeText(this, "TOAST", Toast.LENGTH_LONG)
+
                 toast.show()
+            }
+            R.id.button_snack ->{
+                val snack = Snackbar.make(linear_root, getString(R.string.snack), Snackbar.LENGTH_SHORT)
+                snack.show()
             }
         }
     }

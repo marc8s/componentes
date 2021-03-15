5ee6492769aa71483d41ec5c868a1e9f5aaf2028
Marc8s <marcus.ecomp@gmail.com>
2021-03-15 15:40:32 +0000

    Toast Notification

--- app/src/main/java/com/example/componentes/MainActivity.kt
+++ app/src/main/java/com/example/componentes/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.componentes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_toast.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.button_toast ->{
+                val toast = Toast.makeText(this, "TOAST", Toast.LENGTH_LONG)
+                toast.show()
+            }
+        }
+    }
+
+    private fun toast(str:String){
+        Toast.makeText(this, "TOAST", Toast.LENGTH_LONG).show()
     }
 }
